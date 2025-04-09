b29de40cad4d2d1253bce72c0e12354a29f5c1ea
yourboytony <nigroan67@gmail.com>
2025-04-09 12:29:26 -0700

    fix: add vue-toastification package and configuration

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+
 import App from './App.vue'
 import router from './router'
 
@@ -18,6 +21,24 @@ console.log('Pinia store mounted')
 
 app.use(router)
 console.log('Router mounted')
+
+app.use(Toast, {
+  position: 'top-right',
+  timeout: 3000,
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  draggable: true,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: false,
+  hideProgressBar: true,
+  closeButton: 'button',
+  icon: true,
+  rtl: false,
+  transition: 'Vue-Toastification__bounce',
+  maxToasts: 20,
+  newestOnTop: true
+})
 
 // Add error handler
 app.config.errorHandler = (err, instance, info) => {
