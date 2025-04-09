bcf7b0a924fc98eabf052ecd9bc58c0ac8733a89
yourboytony <nigroan67@gmail.com>
2025-04-09 07:26:48 -0700

    refactor: move Firebase config to environment variables

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,14 +4,14 @@ import { getAuth } from 'firebase/auth'
 import { getAnalytics } from 'firebase/analytics'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBpRkyl6HoR1TQV7HuzBWkkLFKf8hYVXdk",
-  authDomain: "hueseventsdb.firebaseapp.com",
-  databaseURL: "https://hueseventsdb-default-rtdb.firebaseio.com",
-  projectId: "hueseventsdb",
-  storageBucket: "hueseventsdb.firebasestorage.app",
-  messagingSenderId: "285098880882",
-  appId: "1:285098880882:web:e81b7bcf22a67aeeee37eb",
-  measurementId: "G-GJ5XR7CNQY"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 }
 
 console.log('Initializing Firebase with config:', firebaseConfig)
